1d5904ef262eb1e02950269fd890babba523ecc3
joepow82 <joel.neil.powell@yandex.ru>
2023-07-04 23:03:56 +0400

    Seminar3HomeTask

---
+++ Seminar3/Seminar3HomeTask/HW_task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+bool CheckPalindrome(int number)
+{
+    int reversed = 0;
+    int temp = number;
+    while (reversed < number)
+    {
+        reversed = reversed * 10 + temp % 10;
+        temp /= 10;
+    }
+    if (reversed == number) return true;
+    else return false;
+}
+
+Console.Write("Enter a natural number: ");
+int naturalNumber = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckPalindrome(naturalNumber);
+
+Console.WriteLine($"Is number {naturalNumber} a palindrome? -> {result}");
---
+++ Seminar3/Seminar3HomeTask/HW_task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+void WriteCubes(int limit)
+{
+    int counter = 1;
+    while (counter <= limit)
+    {
+        Console.Write($"{Convert.ToInt32(Math.Pow(counter, 3))} ");
+        counter++;
+    }
+}
+
+Console.Write("Enter a natural number: ");
+int naturalNumber = Convert.ToInt32(Console.ReadLine());
+WriteCubes(naturalNumber);
