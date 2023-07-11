3f8525ca6eb115e9d052fb0f0360542f7d07bebb
joepow82 <joel.neil.powell@yandex.ru>
2023-07-11 22:07:48 +0400

    HW_task27

---
+++ Seminar4/Seminar4HomeTask/HW_task27/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigits(int number)
+{
+    int result = 0;
+    int numberCopy = number;
+
+    while (numberCopy != 0)
+    {
+        result += numberCopy % 10;
+        numberCopy /= 10;
+    }
+
+    return result;
+}
+
+Console.Write("Enter a number: ");
+int myNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits in {myNumber} equals {SumDigits(myNumber)}");
