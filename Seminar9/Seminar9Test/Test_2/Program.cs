12c25edbc50e62cdf7f03a4814b40e70338e2cce
joepow82 <joel.neil.powell@yandex.ru>
2023-07-26 21:50:00 +0400

    seminar9_test

---
+++ Seminar9/Seminar9Test/Test_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дан одномерный массив целых однозначных чисел (элемент массива содержит число от 1 до 9).
+//Ваша задача вывести на экран число, которое формируется элементами массива слева-направо + 1
+
+//Method
+int ParseArrayByStdMethod(int[] array){
+    string str = string.Join("", array);
+    Console.WriteLine(str);
+    return int.Parse(str);
+}
+
+int ParseArrayManual(int[] array){
+    string str = string.Join("", array);
+    Console.WriteLine(str);    
+    int result = 0;
+    for (int i = array.Length; i <= 0; i--){
+        result = result + array[i] * Convert.ToInt32(Math.Pow(10,i));
+    }
+    return result;
+}
+
+int[] test = {1,2,1,1,4};
+Console.WriteLine(ParseArrayByStdMethod(test)+1);
+Console.WriteLine();
+int[] test1 = {1,1,2,9};
+Console.WriteLine(ParseArrayByStdMethod(test1)+1);
+Console.WriteLine();
+int[] test2 = {9};
+Console.WriteLine(ParseArrayByStdMethod(test2)+1);
---
+++ Seminar9/Seminar9Test/Test_3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Дана строка, содержащая только следующие символы:'(', ')', '{', '}', '[', ']'. 
+//Определите, является ли ваша строка допустимой.
+
+//Method
+bool CheckBrackets(string str)
+{
+    Console.WriteLine(str);
+
+    Stack<char> brackets = new Stack<char>();
+
+    foreach (char sym in str)
+    {
+        switch (sym)
+        {
+            case '(' or '{' or '[':
+                brackets.Push(sym);
+                break;
+            case ']':
+                if (brackets.Count == 0) return false;
+                if (brackets.Pop() != '[') return false;
+                break;
+            case ')':
+                if (brackets.Count == 0) return false;
+                if (brackets.Pop() != '(') return false;
+                break;
+            case '}':
+                if (brackets.Count == 0) return false;
+                if (brackets.Pop() != '{') return false;         
+                break;
+            default:
+                break;
+        }
+    }
+    if (brackets.Count > 0) return false;
+    return true;
+}
+
+string testStr = "()";
+Console.WriteLine(CheckBrackets(testStr));
+Console.WriteLine();
+
+testStr = "(){}[]";
+Console.WriteLine(CheckBrackets(testStr));
+Console.WriteLine();
+
+testStr = "(]";
+Console.WriteLine(CheckBrackets(testStr));
+Console.WriteLine();
+
+testStr = "()[]{()}";
+Console.WriteLine(CheckBrackets(testStr));
+Console.WriteLine();
